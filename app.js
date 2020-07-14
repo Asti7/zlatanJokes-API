e6358fcc8722680a37990163bf85ad5ba63e3171
Asti7 <astitvagautam@gmail.com>
2020-07-15 08:13:16 +1000

    connected mongodb local instance and populated the db with some data.

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/zlatanJokeDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
+const jokesSchema = new mongoose.Schema({
+  joke: String,
+});
 
-app.listen(3000,()=>{
-    console.log('Server started on port 3000.');
+const Joke = mongoose.model("Joke", jokesSchema);
+
+app.get("/", (req, res) => {
+  Joke.find({}, (err, foundJokes) => {
+    if (!err) {
+      let joke = foundJokes[Math.floor(Math.random() * foundJokes.length)];
+
+      res.send(joke);
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000.");
 });
